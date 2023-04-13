3faee948f3c0e67c9df4f9eaed4b4667b6c665ee
nikkitavr <vorotnikov.np@yandex.ru>
2023-04-13 18:18:14 +0300

    Add eureka-server

---
+++ geot-eureka-server/src/main/java/ru/nikkitavr/geotagger/geoteurekaserver/GeotEurekaServerApplication.java
@@ -0,0 +1,13 @@
+package ru.nikkitavr.geotagger.geoteurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeotEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeotEurekaServerApplication.class, args);
+	}
+
+}
